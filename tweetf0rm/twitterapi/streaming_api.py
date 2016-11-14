5fd7702156f1fc00bc80468b79c78e5161c6d746
danran <danran@datatub.com>
2016-11-14 09:55:42 +0800

    write streaming result to txt file

--- tweetf0rm/twitterapi/streaming_api.py
+++ tweetf0rm/twitterapi/streaming_api.py
@@ -12,45 +12,50 @@ import logging
 logger = logging.getLogger(__name__)
 
 from twython import TwythonStreamer
+from tweetf0rm.exceptions import MissingArgs
 import os, copy, datetime, json
 
+
 class Streamer(TwythonStreamer):
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor with apikeys, and output folder
 
-	def __init__(self, *args, **kwargs):
-		"""
-		Constructor with apikeys, and output folder
+        * apikeys: apikeys
+        """
+        logger.info(kwargs)
+        import copy
 
-		* apikeys: apikeys
-		"""
-		logger.info(kwargs)
-		import copy
+        apikeys = copy.copy(kwargs.pop('apikeys', None))
+        output = copy.copy(kwargs.pop('output', None))
 
-		apikeys = copy.copy(kwargs.pop('apikeys', None))
-		
-		if not apikeys:
-			raise MissingArgs('apikeys is missing')
+        if not apikeys:
+            raise MissingArgs('apikeys is missing')
+        if not output:
+            raise MissingArgs('output is missing')
 
-		self.apikeys = copy.copy(apikeys) # keep a copy
-		
-		kwargs.update(apikeys)
+        self.apikeys = copy.copy(apikeys)  # keep a copy
+        self.output = open(output, 'a')
+        self.counter = 0
 
-		super(Streamer, self).__init__(*args, **kwargs)
+        kwargs.update(apikeys)
 
+        super(Streamer, self).__init__(*args, **kwargs)
 
-	def on_success(self, data):
-		if 'text' in data:
-			self.counter += 1
-			if self.counter % 1000 == 0:
-				logger.info("received: %d"%self.counter)
-			#logger.debug(data['text'].encode('utf-8'))
-			self.write_to_handler.append(json.dumps(data))
+    def on_success(self, data):
+        if 'text' in data:
+            self.counter += 1
+            if self.counter % 1000 == 0:
+                logger.info("received: %d" % self.counter)
+            # logger.debug(data['text'].encode('utf-8'))
+            # self.write_to_handler.append(json.dumps(data))
+            json.dump(data, self.output)
+            self.output.write('\n')
 
-			
-	def on_error(self, status_code, data):
-		 logger.warn(status_code)
+    def on_error(self, status_code, data):
+        logger.warn(status_code)
 
-		
-	def close(self):
-		self.disconnect()
-		self.write_to_handler.close()
-		
+    def close(self):
+        self.disconnect()
+        # self.write_to_handler.close()
+        self.output.close()
